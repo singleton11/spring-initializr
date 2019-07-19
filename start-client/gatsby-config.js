bfc9e046a4bad10aa97d4102551b03d91c7c275e
Anton Prokhorov <betrayer11@gmail.com>
2019-07-19 22:12:16 +0300

    Add perseus icon to header of initializer

--- start-client/gatsby-config.js
+++ start-client/gatsby-config.js
@@ -1,7 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 const siteMetadata = {
-  title: `Spring Initializr`,
+  title: `Perseus Spring Initializr`,
   description: `Initializr generates spring boot project with just what you need to start quickly!`,
   twitter: `@springboot`,
   canonical: `https://start.spring.io`,
